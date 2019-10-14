cdaaab4c42a3dab5171dde4bb23516a474d84df3
AkinWiz2345 <ikki_safety-techacademy@yahoo.co.jp>
2019-10-14 16:06:04 +0000

    users #3

---
+++ db/migrate/20191014054030_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.integer :gender
+      t.date :birth
+      t.integer :area
+      t.integer :part
+      t.text :introduction
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
